a7f649ffdc93cc464ce8816224e3a86f99711874
elsydev <interelsy2@gmail.com>
2025-01-21 11:18:55 -0400

    Update generarJWT.js

--- libs/generarJWT.js
+++ libs/generarJWT.js
@@ -3,9 +3,9 @@ import jwt from "jsonwebtoken";
 const generarJWT = (id) => {
   return jwt.sign({ id }, process.env.SECRET_JWT_WORD, {
     expiresIn: "30d",
-    httpOnly:true,
+    /* httpOnly:true,
     secure:true,
-    sameSite:"None"
+    sameSite:"None" */
   });
 };
 export default generarJWT;
