ef9a8fb53e78fe315cc24a866837269e3a066417
elsydev <interelsy2@gmail.com>
2025-03-09 11:37:46 -0400

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const ACCEPTED_ORIGINS = [
     'http://127.0.0.1:5173/',
     "http://localhost:5174",
     "http://127.0.0.1:5174",
-    "https://heartfelt-clafoutis-4aa53b.netlify.app/",
+    
     "https://auction-mern-fullstack-frontend.vercel.app/",
     "https://auction-mern-fullstack-backend.onrender.com"
      
