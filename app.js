44e27d2e52df18117d5794ba58aba4bef737d265
elsydev <interelsy2@gmail.com>
2025-03-09 11:29:37 -0400

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.json());
 const ACCEPTED_ORIGINS = [
     'http://localhost:5000',
     'http://localhost:4000',
-    'http://localhost:1234',
+    
     'http://localhost:3000',
     'http://localhost:5173',
     'http://127.0.0.1:5173',
